137f9400b33992d63e2d6b231619f691cec209e6
chinmay-ofiicial <154550777+chinmay-ofiicial@users.noreply.github.com>
2024-02-14 11:19:22 +0100

    Update ElectionDataScrape.py

--- ElectionDataScrape.py
+++ ElectionDataScrape.py
@@ -1,97 +1,46 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
+#Importing the necessary libraries.
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
-# In[7]:
-
-
+#Opening the URL in the given web browser (in this case Firefox).
 driver = webdriver.Firefox()
-url = driver.get("https://www.indiavotes.com/pc/party_list/0/17")
-
-
-# In[8]:
-
-
+driver.get("https://www.indiavotes.com/pc/party_list/0/17")
 page_source = driver.page_source
 
-
-# In[9]:
-
-
+#creates a 'Soup' object used to parse the HTML content.
 soup = BeautifulSoup(page_source, 'html.parser')
 
-
-# In[10]:
-
-
+# The 'find' feature is used to locate certain content from the HTML.
 main = soup.find('div', class_='bodyBg').find('div', class_ = 'main').find('div', class_ = 'mianContent').find('div', class_ = 'mapTabData').find('div', class_='dataTables_wrapper')
 
-
-# In[11]:
-
-
+#'find_all' finds all the matching elements compared to 'find' which outputs the first matching element. 
+#Here find_all is used to find the HTML <th>(table header) element
 t1 = main.find_all('th')
 
-
-# In[12]:
-
-
+#Creates the list of table headers extracted from the previous code and removes unwanted headers. 
 header = [title.text.strip() for title in t1]
-
-
-# In[13]:
-
-
 header = header[13:]
-
-
-# In[14]:
-
-
 print(header)
 
-
-# In[15]:
-
-
+#A panda dataframe is created with headers from previous code as columns. Later another column is added to the '0th' position called 'Sr.No.'
 df = pd.DataFrame(columns = header)
 df.insert(0, 'Sr. No.', '')
-df
 
-
-# In[16]:
-
-
+#Individual rows(<tr>) and datapoints(<td>) are extracted and added to the dataframe which matches the length of the original dataframe.
 t2 = main.find_all('tr', class_ = {'odd', 'even'})
-
-
-# In[17]:
-
-
 for row in t2:
     r = row.find_all('td')
     ind_row = [data.text.strip() for data in r]
     #print(ind_row)
     length = len(df)
     df.loc[length] = ind_row
-    
 
 
-# In[19]:
-
-
+#CSV file is created in the end to complete the scraping.
 df.to_csv('Election_2019_Partywise.csv', index=False)
 
-
-# In[ ]:
 
 
 
